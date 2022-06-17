38f4c82818e04ba7891d52fd2a51b8ad016dab85
Anna Bellizzi <>
2022-06-17 18:59:32 +0000

    CAbash.sh is fully automated to create CA and certificates without user input. AB

--- createCA.sh
+++ createCA.sh
@@ -4,6 +4,7 @@
 # for generating your root CA private key and cert
 ##################################################
 
+echo "Create directories"
 sudo rm -rf /etc/ssl/*CA*
 sudo mkdir /etc/ssl/demoCA
 sudo mkdir /etc/ssl/demoCA/certs
@@ -12,31 +13,21 @@ sudo mkdir /etc/ssl/demoCA/private
 
 echo "directories created in /etc/ssl/demoCA"
 ls /etc/ssl/demoCA
-echo ""
-echo ""
 echo "done creating folders"
 echo ""
-echo ""
+echo "Initialize starting serial number"
+sudo touch /etc/ssl/demoCA/index.txt
+sudo echo '1000' > /etc/ssl/demoCA/serial
 
 cd /etc/ssl/demoCA
 
-echo ""
-echo ""
-echo "Generate the CA RSA private key of size 2048 bits that will be encrypted by the Advanced Encryption Standard (AES) using a 256-bit key"
-echo "[Enter] to continue"
-read
+echo "Generate the CA RSA private key that will be encrypted, you will be prompted to create and verify a passphrase"
+openssl genrsa -aes256 -out cakey.pem -passout pass:test 2048
 
-# generate new rsa private key for CA
-openssl genrsa -aes256 -out cakey.pem 2048
-
-echo ""
-echo ""
 echo "Now create the root CA certificate"
-echo "[Enter] to continue"
-read
+
 # create the root CA certificate (this is what you will install in your browser if you are not using intermediary signing keys)
-openssl req -x509 -new -nodes -key cakey.pem -sha256 -days 1825 -out cacert.pem
-# openssl req -x509 -new -config -key cakey.pem -sha256 -days 1825 -out cacert.pem -subj "/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN=ca.cst311.test"
+openssl req -x509 -new -nodes -key cakey.pem -sha256 -days 1825 -out cacert.pem -passin pass:test -subj "/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN=ca.cst311.test"
 
 # display  root cert in default form
 echo ""
@@ -61,23 +52,24 @@ ls /usr/local/share/ca-certificates/cacert.crt
 echo "copid root CA cert to ca-certificates dir and changed extension"
 
 #RUN CA-CERTIFICATES APP
-sudo update-ca-certificates
-
+#sudo update-ca-certificates
+#echo "mark 1"
 # Generate the Server Certificate
 # Generate a new 2048-bit RSA private key for your server
 openssl genrsa -out cst311.webpa4-key.pem 2048
-
-#Generate a certificated newcerts signing request to send to the root CA using the private key generated above
+#echo "mark 2"
+#Generate a certificate signing request to send to the root CA using the private key generated above
 #Auto fill requested information
 openssl req -new -config /etc/ssl/openssl.cnf -key cst311.webpa4-key.pem -out cst311.webpa4.csr -subj "/C=US/ST=CA/L=Seaside/O=CST311/OU=Networking/CN=www.webpa4.test"
-
 #Now use the Root CA to create the X.509 server certificate that is valid for 365 days.  Sign the certificate with the CA certificate
-openssl x509 -req -days 365 -in cst311.webpa4.csr -CA cacert.pem -CAkey ./private/cakey.pem -CAcreateserial -out cst311.webpa4-cert.pem
+openssl x509 -req -days 365 -in cst311.webpa4.csr -CA cacert.pem -CAkey ./private/cakey.pem -CAcreateserial -out cst311.webpa4-cert.pem -passin pass:test
 
 echo "You have created a server certificate, that is valid for one year, and signed it with your CA certificate"
-
+echo "mark 4"
 #display decrypted server cert
 echo "Decrypted Server Cert"
 echo "GET A SCREEN SHOT FOR SUBMISSION"
 openssl x509 -text -noout -in cst311.webpa4-cert.pem
-
+cd /etc/ssl/demoCA
+sudo mv cst311.webpa4-cert.pem newcerts
+sudo mv cst311.webpa4-key.pem private
